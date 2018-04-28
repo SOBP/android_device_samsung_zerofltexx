adefbcf0deb953bcda2f1895f7d5cad1a7ea919f
enesuzun2002 <4lukankalar@gmail.com>
2018-04-28 17:23:25 +0000

    zerofltexx: Commonize stuff
    Change-Id: Iff47a75c2cbc8b48afd17310cec83d2d71ce02b0

--- device.mk
+++ device.mk
@@ -18,21 +18,9 @@ LOCAL_PATH := device/samsung/zerofltexx
 
 ## device overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
     
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/zerofltexx/zerofltexx-vendor.mk)
-
-# Carrier init
-PRODUCT_PACKAGES += \
-    init.carrier.rc
-
-# cpboot daemon
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
 
 # Inherit from zero-common
 $(call inherit-product, device/samsung/zero-common/zero-common.mk)
