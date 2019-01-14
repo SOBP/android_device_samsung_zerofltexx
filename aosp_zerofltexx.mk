a32afc9e87593fcfbb16b96533b7174a59040ed7
SOBP <42330344+SOBP@users.noreply.github.com>
2019-01-14 13:23:35 +0000

    Update aosp_zerofltexx.mk

--- aosp_zerofltexx.mk
+++ aosp_zerofltexx.mk
@@ -7,17 +7,8 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/aosp/config/common.mk)
 
-PRODUCT_# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm \
-    vendor.lineage.livedisplay-V1.0-java
-
-PRODUCT_BOOT_JARS += \
-    vendor.lineage.livedisplay-V1.0-java
-BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltexx" \
     TARGET_DEVICE="zeroflte"
 
 PRODUCT_NAME := aosp_zerofltexx
 PRODUCT_DEVICE := zerofltexx
-WITH_DEXPREOPT := false
